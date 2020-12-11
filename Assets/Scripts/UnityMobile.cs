57122b54d3f99922b6c85bb1de8919e115d8381e
demonbeduin <team.sad.info@gmail.com>
2020-12-11 13:04:36 +0300

    test1

---
+++ Assets/Scripts/UnityMobile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityMobile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.autorotateToPortrait = true;
+
+        Screen.autorotateToPortraitUpsideDown = true;
+
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
